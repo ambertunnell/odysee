d5abbfe35b9f04df03ced259bc341794a21bed9d
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-13 14:51:30 -0400

    changes route order

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,15 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :locations, :only => [:index, :create]
-  resources :days, :only => [:index, :create, :show, :destroy]
-
-  post 'days/destroy_locations' 
-
-  resources :users
-
   root 'users#show'
-  
+
   get '/login' => 'sessions#new'
   get '/auth/facebook/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+  post 'days/destroy_locations' 
+
+  resources :locations, :only => [:index, :create]
+  resources :days, :only => [:index, :create, :show, :destroy]
+  resources :users
 
 end
